656269d09d152cbee22999a0c3601d21c7090377
dannypas00 <dannypas00@outlook.com>
2019-03-17 23:55:13 +0100

    Initialized project

---
+++ VS Project/Pokerapp/Pokerapp/Queries/InitiateDatabase.sql
@@ -0,0 +1,14 @@
+﻿DROP TABLE Room;
+CREATE TABLE Room
+(
+	[Room_Code] INT NOT NULL PRIMARY KEY,
+	[Nr_Of_Players] INT NOT NULL,
+	[Tbl_Min_Size] INT NOT NULL,
+	[Started] CHAR(1) NOT NULL,
+	[Chip_1_Worth] INT NOT NULL,
+	[Chip_2_Worth] INT NOT NULL,
+	[Chip_3_Worth] INT NOT NULL,
+	[Chip_4_Worth] INT NOT NULL,
+	[Chip_5_Worth] INT NOT NULL,
+	[Round_Time] TIME NOT NULL
+);
