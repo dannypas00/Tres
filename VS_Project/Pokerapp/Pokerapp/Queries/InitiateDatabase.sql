75e0102e60df9db698c7a670acf0c01ad01d2511
Jan Minne <janminne@gmail.com>
2019-03-29 12:52:56 +0100

    Tableview with:
    Delete & Rebuy players - no AJAX
    End game query - waiting for Timer

--- VS_Project/Pokerapp/Pokerapp/Queries/InitiateDatabase.sql
+++ VS_Project/Pokerapp/Pokerapp/Queries/InitiateDatabase.sql
@@ -10,24 +10,26 @@ CREATE TABLE Players
 
 
 DROP TABLE Room;
-CREATE TABLE Room
+CREATE TABLE Room 
 (
-    [Room_Code] INT NOT NULL PRIMARY KEY,
-    [Nr_Of_Players] INT NOT NULL,
-    [Tbl_Min_Size] INT NOT NULL,
-    [Started] CHAR(1) NOT NULL,
-    [Chip_1_Worth] INT NOT NULL,
-    [Chip_2_Worth] INT NOT NULL,
-    [Chip_3_Worth] INT NOT NULL,
-    [Chip_4_Worth] INT NOT NULL,
-    [Chip_5_Worth] INT NOT NULL,
-    [Round_Time] TIME NOT NULl,
-    [Pause_Time] TIME NULL,
-	[Blind_Time] TIME NULL,
-    [Allow_Rebuy] INT NOT NULL,
-    [Start_Value] INT NOT NULL,
-    [Tbl_Amount] INT NOT NULL,
-    [Tbl_Max_Size] INT NOT NULL, 
-	[Small_Blind] INT NOT NULL,
-	[Big_Blind] INT NOT NULL,
+    [Room_Code]     INT      NOT NULL,
+    [Tbl_Min_Size]  INT      NOT NULL,
+    [Started]       CHAR (1) NOT NULL,
+    [Chip_1_Worth]  INT      NOT NULL,
+    [Chip_2_Worth]  INT      NOT NULL,
+    [Chip_3_Worth]  INT      NOT NULL,
+    [Chip_4_Worth]  INT      NOT NULL,
+    [Chip_5_Worth]  INT      NOT NULL,
+    [Round_Time]    TIME (7) NOT NULL,
+    [Pause_Time]    TIME (7) NULL,
+    [Blind_Time]    TIME (7) NULL,
+    [Allow_Rebuy]   INT      NOT NULL,
+    [Start_Value]   INT      NOT NULL,
+    [Tbl_Amount]    INT      NOT NULL,
+    [Tbl_Max_Size]  INT      NOT NULL,
+	[Blind_Round]	INT		 NOT NULL,
+    [Small_Blind]   INT      NOT NULL,
+    [Big_Blind]     INT      NOT NULL,
+    PRIMARY KEY CLUSTERED ([Room_Code] ASC)
 );
+
