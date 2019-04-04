cd79f3fa94351d6b64db8af9af1e58748bf75c5f
Jan Minne <janminne@gmail.com>
2019-04-04 12:18:04 +0200

    ok

--- VS_Project/Pokerapp/Pokerapp/Queries/InitiateDatabase.sql
+++ VS_Project/Pokerapp/Pokerapp/Queries/InitiateDatabase.sql
@@ -5,46 +5,13 @@ CREATE TABLE Players
     [Room_Code] INT NOT NULL, 
     [Tbl_nr] INT NULL, 
     [Rebuy] INT NULL,
-	[Active] INT NULL,
+    [Active] INT NULL,
     PRIMARY KEY ([Name],[Room_Code]), 
 )
 
-
 DROP TABLE Room;
-CREATE TABLE Room 
+CREATE TABLE Room
 (
-<<<<<<< HEAD
-    [Room_Code]     INT      NOT NULL,
-    [Tbl_Min_Size]  INT      NOT NULL,
-    [Started]       CHAR (1) NOT NULL,
-    [Chip_1_Worth]  INT      NOT NULL,
-    [Chip_2_Worth]  INT      NOT NULL,
-    [Chip_3_Worth]  INT      NOT NULL,
-    [Chip_4_Worth]  INT      NOT NULL,
-    [Chip_5_Worth]  INT      NOT NULL,
-    [Round_Time]    TIME (7) NOT NULL,
-    [Pause_Time]    TIME (7) NULL,
-    [Blind_Time]    TIME (7) NULL,
-    [Allow_Rebuy]   INT      NOT NULL,
-    [Start_Value]   INT      NOT NULL,
-    [Tbl_Amount]    INT      NOT NULL,
-    [Tbl_Max_Size]  INT      NOT NULL,
-	[Blind_Round]	INT		 NOT NULL,
-    [Small_Blind]   INT      NOT NULL,
-    [Big_Blind]     INT      NOT NULL,
-    PRIMARY KEY CLUSTERED ([Room_Code] ASC)
-);
-
-DROP TABLE Blinds;
-CREATE TABLE Blinds
-(
-[Room_Code] INT NOT NULL,
-[ID] INT NOT NULL,
-[S_B] CHAR(1) NOT NULL,
-[Value] INT NOT NULL,
-PRIMARY KEY ([Room_Code],[ID], [S_B]), 
-);
-=======
     [Room_Code] INT NOT NULL PRIMARY KEY,
 	[Room_Name] NVARCHAR(50) NOT NULL ,
 	[Room_Create_Date] DATETIME NOT NULL,
@@ -58,6 +25,7 @@ PRIMARY KEY ([Room_Code],[ID], [S_B]),
     [Round_Time] TIME NOT NULl,
     [Pause_Time] TIME NULL,
 	[Blind_Time] TIME NULL,
+	[Started_Time] DATETIME NULL,
     [Allow_Rebuy] INT NOT NULL,
     [Start_Value] INT NOT NULL,
     [Tbl_Amount] INT NOT NULL,
@@ -75,4 +43,3 @@ CREATE TABLE [dbo].[Blinds] (
     [ValueBB] INT NOT NULL, 
     PRIMARY KEY CLUSTERED ([Room_Code] ASC, [ID] ASC)
 );
->>>>>>> master
